265d11a9add019a4662817354531ccd7d993e697
Eric Sun <eric@ubuntu-server-14.04>
2014-05-14 23:49:08 -0400

    Pages have things now.

--- tornadoserver.py
+++ tornadoserver.py
@@ -1,8 +1,27 @@
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
+from tornado.web import FallbackHandler, RequestHandler, Application
+from tornado.websocket import WebSocketHandler
 from labdemo import app
 
-http_server = HTTPServer(WSGIContainer(app))
-http_server.listen(5000)
-IOLoop.instance().start()
+flask_app = WSGIContainer(app)
+
+class PingPongSocket(WebSocketHandler):
+    def open(self):
+        print('new websocket connection')
+
+    def on_message(self, message):
+        print('message')
+
+    def on_close(self):
+        print('websocket connection closed')
+
+application = Application([
+    (r'/ws', PingPongSocket),
+    (r'.*', FallbackHandler, dict(fallback=flask_app))
+    ], debug=True, autoreload=True)
+
+if __name__ == '__main__':
+    application.listen(5000, address='0.0.0.0')
+    IOLoop.instance().start()
