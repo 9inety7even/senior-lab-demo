bb2ab5cd9487a4a58194140ae84ac7807e1b8f0c
Eric Sun <9inety7even@gmail.com>
2014-05-20 13:56:49 -0400

    Added some logging.

--- tornadoserver.py
+++ tornadoserver.py
@@ -3,28 +3,34 @@ from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from tornado.web import FallbackHandler, RequestHandler, Application
 from tornado.websocket import WebSocketHandler
+import logging
 from labdemo import app
 
 flask_app = WSGIContainer(app)
 
 class PingPongSocket(WebSocketHandler):
     def open(self):
-        print('new websocket connection')
+        logging.debug('New websocket connection opened.')
 
     def on_message(self, message):
-        print(message)
         if(str(message) == 'Ping'):
+            logging.debug('"Ping" received, "Pong" sent.')
             self.write_message('Pong')
-        if(str(message) == 'Pong'):
+        elif(str(message) == 'Pong'):
+            logging.debug('"Pong" received, "Ping" sent.')
             self.write_message('Ping')
+        else:
+            logging.debug('Unknown messaged received, nothing sent.')
 
     def on_close(self):
-        print('websocket connection closed')
+        logging.debug('Existing websocket connection closed.')
 
 application = Application([
     (r'/ws', PingPongSocket),
     (r'.*', FallbackHandler, dict(fallback=flask_app))
     ], debug=True, autoreload=True)
+
+logging.basicConfig(format='[%(asctime)s] %(message)s', level=logging.DEBUG)
 
 if __name__ == '__main__':
     application.listen(5000, address='0.0.0.0')
